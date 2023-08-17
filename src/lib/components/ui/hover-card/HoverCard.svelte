6637f62ad0efe80f7e737d8a20611081e230f86b
Tomek Sosinski <tomek.sosinski@gmail.com>
2023-08-17 18:30:28 +0200

    Add HoverCard

---
+++ src/lib/components/ui/hover-card/HoverCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" context="module">
+	import { getContext, setContext } from 'svelte'
+	import type { HoverCard } from '@melt-ui/svelte'
+
+	type HoverCardContest = HoverCard
+
+	function setHoverCardContext(hoverCard: HoverCardContest) {
+		setContext('hover-card', hoverCard)
+	}
+
+	export function getHoverCardContext(): HoverCardContest {
+		return getContext('hover-card')
+	}
+</script>
+
+<script lang="ts">
+	import { createHoverCard, melt } from '@melt-ui/svelte'
+	import { Trigger, Content } from './'
+
+	const hoverCard = createHoverCard({
+		openDelay: 350
+	})
+
+	setHoverCardContext(hoverCard)
+
+	const {
+		elements: { arrow },
+		states: { open }
+	} = hoverCard
+</script>
+
+<slot {Trigger} />
+{#if open}
+	<slot name='content' {Content} />
+	<div use:melt={$arrow} />
+{/if}
---
+++ src/lib/components/ui/hover-card/Trigger.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { getHoverCardContext} from './HoverCard.svelte'
+  import {melt} from '@melt-ui/svelte'
+
+  import {Button} from '$components/ui/button'
+
+  const {elements: {trigger}} = getHoverCardContext()
+
+</script>
+
+<button use:melt={$trigger}>
+  <slot {Button}/>
+</button>
